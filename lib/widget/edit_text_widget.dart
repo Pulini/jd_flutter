63b75dc5543669881afbef6fd6615a9698b039eb
潘卓旭 <644173944@qq.com>
2025-09-02 16:28:40 +0800

    build:升级 JDK 版本并优化文本输入框组件- 将 JDK 版本从 1.8 升级到 11 - 更新 Kotlin版本从 1.8.22 到 2.1.0 - 重构 EditText、NumberDecimalEditText 和 NumberEditText 组件，改为 StatefulWidget - 优化文本输入框的初始化和生命周期管理- 调整路由配置中的材料车间派工页面路由 ID

--- lib/widget/edit_text_widget.dart
+++ lib/widget/edit_text_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:jd_flutter/utils/utils.dart';
 
 //文本输入框
-class EditText extends StatelessWidget {
+class EditText extends StatefulWidget {
   const EditText({
     super.key,
     this.hint,
@@ -24,22 +24,44 @@ class EditText extends StatelessWidget {
   final TextEditingController? controller;
 
   @override
-  Widget build(BuildContext context) {
-    TextEditingController controller = TextEditingController(text: initStr);
-    FocusNode? fn;
-    if (hasFocus) {
-      fn = FocusNode()..requestFocus();
+  State<EditText> createState() => _EditTextState();
+}
+
+class _EditTextState extends State<EditText> {
+  late TextEditingController _controller;
+  FocusNode? _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController(text: widget.initStr);
+    if (widget.hasFocus) {
+      _focusNode = FocusNode()..requestFocus();
     }
+  }
+
+  @override
+  void dispose() {
+    // 只有当我们自己创建controller时才需要dispose
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    _focusNode?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(5),
       height: 40,
       child: TextField(
-        enabled: isEnable,
-        controller: this.controller ?? controller,
+        enabled: widget.isEnable,
+        controller: _controller,
         onChanged: (v) {
-          onChanged?.call(v);
+          widget.onChanged?.call(v);
         },
-        focusNode: fn,
+        focusNode: _focusNode,
         decoration: InputDecoration(
           contentPadding: const EdgeInsets.only(
             top: 0,
@@ -58,15 +80,17 @@ class EditText extends StatelessWidget {
           border: const OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(20)),
           ),
-          labelText: hint,
+          labelText: widget.hint,
           labelStyle: const TextStyle(color: Colors.black54),
-          suffixIcon:hasClose? IconButton(
-            icon: const Icon(Icons.close, color: Colors.grey),
-            onPressed: () {
-              onChanged?.call('');
-              (this.controller ?? controller).clear();
-            },
-          ):null,
+          suffixIcon: widget.hasClose
+              ? IconButton(
+                  icon: const Icon(Icons.close, color: Colors.grey),
+                  onPressed: () {
+                    widget.onChanged?.call('');
+                    _controller.clear();
+                  },
+                )
+              : null,
         ),
       ),
     );
@@ -74,7 +98,7 @@ class EditText extends StatelessWidget {
 }
 
 //数字小数输入框输入框
-class NumberDecimalEditText extends StatelessWidget {
+class NumberDecimalEditText extends StatefulWidget {
   const NumberDecimalEditText({
     super.key,
     this.hasDecimal = true,
@@ -86,7 +110,7 @@ class NumberDecimalEditText extends StatelessWidget {
     this.resetQty = 0,
     this.onChanged,
     this.controller,
-    this.inputEnable=true,
+    this.inputEnable = true,
   });
 
   final String? hint;
@@ -101,62 +125,84 @@ class NumberDecimalEditText extends StatelessWidget {
   final bool? inputEnable;
 
   @override
-  Widget build(BuildContext context) {
-    var c = TextEditingController(
-      text: initQty.toShowString(),
-    );
-    c.selection = TextSelection.fromPosition(
-      TextPosition(offset: c.text.length),
+  State<NumberDecimalEditText> createState() => _NumberDecimalEditTextState();
+}
+
+class _NumberDecimalEditTextState extends State<NumberDecimalEditText> {
+  late TextEditingController _controller;
+  FocusNode? _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController(text: widget.initQty?.toShowString() ?? '');
+    _controller.selection = TextSelection.fromPosition(
+      TextPosition(offset: _controller.text.length),
     );
 
-    if (controller != null) {
-      controller!.selection = TextSelection.fromPosition(
-        TextPosition(offset: controller!.text.length),
-      );
+    if (widget.hasFocus) {
+      _focusNode = FocusNode()..requestFocus();
     }
-    FocusNode? fn;
-    if (hasFocus) {
-      fn = FocusNode()..requestFocus();
+  }
+
+  @override
+  void dispose() {
+    // 只有当我们自己创建controller时才需要dispose
+    if (widget.controller == null) {
+      _controller.dispose();
     }
+    _focusNode?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       height: 40,
       margin: const EdgeInsets.all(5),
       child: TextField(
-        enabled: inputEnable,
+        enabled: widget.inputEnable,
         keyboardType: TextInputType.number,
         inputFormatters: [
-          hasDecimal
+          widget.hasDecimal
               ? FilteringTextInputFormatter.allow(RegExp('[0-9.]')) //数字包括小数
               : FilteringTextInputFormatter.digitsOnly,
         ],
-        focusNode: fn,
-        controller: controller ?? c,
+        focusNode: _focusNode,
+        controller: _controller,
         onChanged: (v) {
-          if(!v.endsWith('.')){
-            if (v.toDoubleTry() > max!) {
-              if (controller != null) {
-                controller!.text = max.toShowString();
-                controller!.selection = TextSelection.fromPosition(
-                  TextPosition(offset: controller!.text.length),
+          if (!v.endsWith('.')) {
+            if (double.tryParse(v) != null && v.toDoubleTry() > widget.max!) {
+              if (widget.controller != null) {
+                widget.controller!.text = widget.max.toShowString();
+                widget.controller!.selection = TextSelection.fromPosition(
+                  TextPosition(offset: widget.controller!.text.length),
                 );
               } else {
-                c.text = max.toShowString();
-                c.selection = TextSelection.fromPosition(
-                  TextPosition(offset: c.text.length),
+                _controller.text = widget.max.toShowString();
+                _controller.selection = TextSelection.fromPosition(
+                  TextPosition(offset: _controller.text.length),
                 );
               }
-              onChanged?.call(max!);
+              widget.onChanged?.call(widget.max!);
             } else {
-              c.text = v.toDoubleTry().toShowString();
-              c.selection = TextSelection.fromPosition(
-                TextPosition(offset: c.text.length),
-              );
-              onChanged?.call(v.toDoubleTry());
+              if (widget.controller != null) {
+                widget.controller!.text = v.toDoubleTry().toShowString();
+                widget.controller!.selection = TextSelection.fromPosition(
+                  TextPosition(offset: widget.controller!.text.length),
+                );
+              } else {
+                _controller.text = v.toDoubleTry().toShowString();
+                _controller.selection = TextSelection.fromPosition(
+                  TextPosition(offset: _controller.text.length),
+                );
+              }
+              widget.onChanged?.call(v.toDoubleTry());
             }
           }
         },
         decoration: InputDecoration(
-          helperText: helperText,
+          helperText: widget.helperText,
           helperStyle: const TextStyle(color: Colors.blueAccent),
           contentPadding: const EdgeInsets.only(
             top: 0,
@@ -175,23 +221,27 @@ class NumberDecimalEditText extends StatelessWidget {
           border: const OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(20)),
           ),
-          labelText: hint,
-          prefixIcon: resetQty!=0?IconButton(
-            onPressed: () {
-              controller == null ? c.text=resetQty.toShowString() : controller?.text=resetQty.toShowString();
-              onChanged?.call(resetQty);
-            },
-            icon: const Icon(
-              Icons.replay_circle_filled,
-              color: Colors.blue,
-            ),
-          ):null,
+          labelText: widget.hint,
           labelStyle: const TextStyle(color: Colors.black54),
+          prefixIcon: widget.resetQty != 0
+              ? IconButton(
+                  onPressed: () {
+                    widget.controller == null
+                        ? _controller.text = widget.resetQty.toShowString()
+                        : widget.controller?.text = widget.resetQty.toShowString();
+                    widget.onChanged?.call(widget.resetQty);
+                  },
+                  icon: const Icon(
+                    Icons.replay_circle_filled,
+                    color: Colors.blue,
+                  ),
+                )
+              : null,
           suffixIcon: IconButton(
             icon: const Icon(Icons.close, color: Colors.grey),
             onPressed: () {
-              controller == null ? c.clear() : controller?.clear();
-              onChanged?.call(0);
+              widget.controller == null ? _controller.clear() : widget.controller?.clear();
+              widget.onChanged?.call(0);
             },
           ),
         ),
@@ -201,7 +251,7 @@ class NumberDecimalEditText extends StatelessWidget {
 }
 
 //数字输入框输入框
-class NumberEditText extends StatelessWidget {
+class NumberEditText extends StatefulWidget {
   const NumberEditText({
     super.key,
     this.hint,
@@ -220,23 +270,43 @@ class NumberEditText extends StatelessWidget {
   final TextEditingController? controller;
 
   @override
-  Widget build(BuildContext context) {
-    var c = TextEditingController(
-      text: initQty.toString(),
-    );
-    FocusNode? fn;
-    if (hasFocus) {
-      fn = FocusNode()..requestFocus();
+  State<NumberEditText> createState() => _NumberEditTextState();
+}
+
+class _NumberEditTextState extends State<NumberEditText> {
+  late TextEditingController _controller;
+  FocusNode? _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController(text: widget.initQty.toString());
+    if (widget.hasFocus) {
+      _focusNode = FocusNode()..requestFocus();
     }
+  }
+
+  @override
+  void dispose() {
+    // 只有当我们自己创建controller时才需要dispose
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    _focusNode?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       height: 40,
       margin: const EdgeInsets.all(5),
       child: TextField(
-        focusNode: fn,
+        focusNode: _focusNode,
         keyboardType: TextInputType.number,
         inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-        controller: controller ?? c,
-        onChanged: onChanged,
+        controller: _controller,
+        onChanged: widget.onChanged,
         decoration: InputDecoration(
           contentPadding: const EdgeInsets.only(
             top: 0,
@@ -255,15 +325,17 @@ class NumberEditText extends StatelessWidget {
           border: const OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(20)),
           ),
-          labelText: hint,
+          labelText: widget.hint,
           labelStyle: const TextStyle(color: Colors.black54),
-          suffixIcon: showClean?IconButton(
-            icon: const Icon(Icons.close, color: Colors.grey),
-            onPressed: (){
-              (controller ?? c).clear();
-              onChanged?.call('');
-            },
-          ):Container(),
+          suffixIcon: widget.showClean
+              ? IconButton(
+                  icon: const Icon(Icons.close, color: Colors.grey),
+                  onPressed: () {
+                    _controller.clear();
+                    widget.onChanged?.call('');
+                  },
+                )
+              : Container(),
         ),
       ),
     );
