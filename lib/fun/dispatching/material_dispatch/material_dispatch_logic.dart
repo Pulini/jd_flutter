daa01ca881b2a20e27d4f45ebd3eae003674300f
Ajay <519419468@qq.com>
2025-10-07 14:37:58 +0800

    无用代码

--- lib/fun/dispatching/material_dispatch/material_dispatch_logic.dart
+++ lib/fun/dispatching/material_dispatch/material_dispatch_logic.dart
@@ -7,7 +7,6 @@ import 'package:jd_flutter/utils/extension_util.dart';
 import 'package:jd_flutter/utils/printer/print_util.dart';
 import 'package:jd_flutter/utils/printer/tsc_util.dart';
 import 'package:jd_flutter/utils/utils.dart';
-import 'package:jd_flutter/utils/web_api.dart';
 import 'package:jd_flutter/widget/custom_widget.dart';
 import 'package:jd_flutter/widget/dialogs.dart';
 import 'package:jd_flutter/widget/preview_label_widget.dart';
