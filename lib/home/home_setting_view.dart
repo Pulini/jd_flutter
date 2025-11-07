a4b2829f8d1f52da6c82e029ca90c23f0b21c3ae
潘卓旭 <644173944@qq.com>
2025-11-07 16:25:11 +0800

    - 移除了 home_setting_view.dart 中未使用的 translation 导入

--- lib/home/home_setting_view.dart
+++ lib/home/home_setting_view.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart' hide FormData;
 import 'package:jd_flutter/bean/http/response/department_info.dart';
 import 'package:jd_flutter/constant.dart';
 import 'package:jd_flutter/login/login_view.dart';
-import 'package:jd_flutter/translation.dart';
 import 'package:jd_flutter/utils/app_init.dart';
 import 'package:jd_flutter/utils/utils.dart';
 import 'package:jd_flutter/widget/custom_widget.dart';
