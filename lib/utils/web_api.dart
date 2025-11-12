e476bda1051659b1301945d862017cea65ff3dd1
潘卓旭 <644173944@qq.com>
2025-11-12 10:40:29 +0800

    chore(release): 更新应用版本至2.2.6
    - 将构建模式从 debug 更改为 release
    - 更新版本号 flutter.versionName 为2.2.6
    - 更新版本代码 flutter.versionCode 为226
    - 在 pubspec.yaml 中更新 version 字段为 2.2.6+226
    - 添加 path、sqflite、image、device_info_plus 和 native_device_orientation 依赖- 移除重复和注释掉的依赖声明- 调整 MES 测试库 URL 配置
    - 切换 SAP 开发库 URL 至 S4-300 环境

--- lib/utils/web_api.dart
+++ lib/utils/web_api.dart
@@ -29,8 +29,8 @@ const resultToUpdate = 3;
 const baseUrlForMES = 'https://geapp.goldemperor.com:1226/';
 
 //MES测试库
-const testUrlForMES = 'https://geapptest.goldemperor.com:1224/';//ECC-300
-// const testUrlForMES = 'https://apptest.goldemperor.com:1207/';//S4-300
+// const testUrlForMES = 'https://geapptest.goldemperor.com:1224/';//ECC-300
+const testUrlForMES = 'https://apptest.goldemperor.com:1207/';//S4-300
 // const testUrlForMES = 'https://apptest.goldemperor.com:1208/';//S4-600
 
 //SAP正式库
@@ -41,8 +41,8 @@ const baseUrlForSAP = 'https://erpprd01.goldemperor.com:8003/';//ECC-800
 // const developUrlForSAP = 'https://s4qasapp01.goldemperor.com:8006/';//S4-600
 
 //SAP开发库
-const developUrlForSAP = 'https://erpdev01.goldemperor.com:8001/';//ECC-300
-// const developUrlForSAP = 'https://s4devapp01.goldemperor.com:8005/';//S4-300
+// const developUrlForSAP = 'https://erpdev01.goldemperor.com:8001/';//ECC-300
+const developUrlForSAP = 'https://s4devapp01.goldemperor.com:8005/';//S4-300
 
 
 const sfUser = 'PI_USER';
