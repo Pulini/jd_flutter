a7391069bfce13b942fc9bc95517ea9a279102c8
Ajay <519419468@qq.com>
2025-12-30 15:50:57 +0800

    无效代码

--- lib/fun/warehouse/out/material_label_scan/material_label_scan_detail_view.dart
+++ lib/fun/warehouse/out/material_label_scan/material_label_scan_detail_view.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
 import 'package:jd_flutter/fun/warehouse/out/material_label_scan/material_label_scan_logic.dart';
 import 'package:jd_flutter/widget/custom_widget.dart';
