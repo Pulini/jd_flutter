9f2c4ef67f3e72a319a42e51540f056f5c86fed3
Ajay <519419468@qq.com>
2026-01-13 16:28:04 +0800

    品检单版本+1

--- lib/route.dart
+++ lib/route.dart
@@ -570,7 +570,7 @@ class RouteConfig {
   //品检单列表
   static Routes qualityInspectionList = Routes(
     '/quality_inspection_list',
-    204,
+    205,
     const QualityInspectionListPage(),
   );
 
