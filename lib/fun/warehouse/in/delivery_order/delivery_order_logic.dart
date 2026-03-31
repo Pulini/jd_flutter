6ce3f6cecd1dd405ede263ca33584a847fbebfa4
Ajay <519419468@qq.com>
2026-03-31 14:18:41 +0800

    删除冗余代码。

--- lib/fun/warehouse/in/delivery_order/delivery_order_logic.dart
+++ lib/fun/warehouse/in/delivery_order/delivery_order_logic.dart
@@ -8,7 +8,6 @@ import 'package:jd_flutter/utils/utils.dart';
 import 'package:jd_flutter/widget/custom_widget.dart';
 import 'package:jd_flutter/widget/dialogs.dart';
 
-import '../../../../utils/web_api.dart';
 import 'delivery_order_label_binding_view.dart';
 import 'delivery_order_state.dart';
 
