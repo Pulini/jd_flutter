65f4c04d1f83eeae97d229aa20f718a3eafd7b09
潘卓旭 <644173944@qq.com>
2026-05-25 09:22:49 +0800

    feat: 更新部件贴标模块并国际化
    重构包装清单数据模型，新增包装规格修改功能，替换硬编码中文为国际化翻译键，使用AutoSizeText自适应按钮文字，修复测试环境背景色逻辑反转问题。

--- lib/fun/warehouse/manage/part_dispatch_label_manage/pack_order_list/pack_order_list_logic.dart
+++ lib/fun/warehouse/manage/part_dispatch_label_manage/pack_order_list/pack_order_list_logic.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:jd_flutter/bean/http/response/pack_order_list_info.dart';
 import 'package:jd_flutter/utils/utils.dart';
