8b3e54fcb3e3367d2c242d1ef488e2efc1a88147
Ajay <519419468@qq.com>
2026-07-27 15:08:55 +0800

    无效代码删除

--- lib/fun/warehouse/manage/new_carton_label_scan/new_carton_label_scan_priority_view.dart
+++ lib/fun/warehouse/manage/new_carton_label_scan/new_carton_label_scan_priority_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jd_flutter/bean/http/response/carton_label_scan_info.dart';
-import 'package:jd_flutter/utils/web_api.dart';
-
 import 'package:jd_flutter/widget/combination_button_widget.dart';
 import 'package:jd_flutter/widget/custom_widget.dart';
 import 'package:jd_flutter/widget/dialogs.dart';
